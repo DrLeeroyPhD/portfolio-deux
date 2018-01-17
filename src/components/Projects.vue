1d40eeaa0e6ec20028b419da83434c1b6c17c453
DrLeeroyPhD <dennyjchristopher@gmail.com>
2018-01-16 18:28:04 -0800

    Added general layout to projects section

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -2,8 +2,16 @@
   <div id="Projects">
    
    
-    <img src="https://www.fillmurray.com/300/300" alt="Fill Murray" />
-
+    <div><img src="https://www.fillmurray.com/300/300" alt="Fill Murray" class="myWork" /></div>
+    
+    <div id="projectGallery">
+      <a href="#"><img src="http://via.placeholder.com/350x250" alt=""></a>
+      <a href="#"><img src="http://via.placeholder.com/350x250" alt=""></a>
+      <a href="#"><img src="http://via.placeholder.com/350x250" alt=""></a>
+      <a href="#"><img src="http://via.placeholder.com/350x250" alt=""></a>
+      <a href="#"><img src="http://via.placeholder.com/350x250" alt=""></a>
+      <a href="#"><img src="http://via.placeholder.com/350x250" alt=""></a>
+    </div>
     
   </div>
 </template>
@@ -26,9 +34,20 @@ export default {
     margin-top: 200px;
     width: 90%;
     margin-left: 5%;
+    display: grid;
+    grid-template-columns: 1fr 9fr;
+    grid-template-rows: 1fr;
+    align-items: center;
   }
   
-  #Projects img{
+  #projectGallery{
+    display: grid;
+    grid-template-rows: 1fr 1fr;
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+  
+  img.myWork{
+    margin-right: 10px;
     border-radius: 50%;
   }
   
